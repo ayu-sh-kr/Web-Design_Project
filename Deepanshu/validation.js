bb627c23011e5786db0a5151856221a64f535124
ayu-sh-kr <akjaiswal2003@gmail.com>
2023-06-23 13:08:10 +0530

    improved code quality

--- Deepanshu/validation.js
+++ Deepanshu/validation.js
@@ -7,13 +7,19 @@
 function validation(){
     let uname=document.getElementById("username");
     let pdetail=document.getElementById("password");
+    const regx = new RegExp("/^[A_Za-z]/");
     console.log("Dun")
     if(uname.value.trim() === ""){
         alert("Credentials can't be empty")
         console.log("Hi")
-        // uname.value="Username is must";
+        document.getElementById("error").innerHTML="Username is must";
         return false;
     }
+    else if(!regx.test(uname.value)){
+        alert("Username can't contain numeric value")
+        return false
+    }
+
     else if(pdetail.value.trim().length<6){
         pdetail.value="password is incorrect";
         return false;
